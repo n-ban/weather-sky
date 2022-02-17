80addca2cddf5ae33714863e5bbe80786800676a
n-ban <59346245+n-ban@users.noreply.github.com>
2022-02-17 10:45:33 +0300

    Add files via upload

---
+++ TestApi/Controllers/HealthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class HealthController : ControllerBase
+    {
+        private readonly ILogger<HealthController> _logger;
+
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public HealthDto Check()
+        {
+            var httpConnectionFeature = HttpContext.Features.Get<IHttpConnectionFeature>();
+            var localIpAddress = httpConnectionFeature?.LocalIpAddress;
+            var localPort = httpConnectionFeature?.LocalPort;
+            var serverId = ServerInstance.Id;
+
+            return new HealthDto(serverId.ToString(), localIpAddress.ToString(), localPort.ToString());
+        }
+
+    }
+}
